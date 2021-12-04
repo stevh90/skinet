604dd6a81574ce572c9e5205cdb8afa0e49f3b3d
Steven Heath <stevh90@gmail.com>
2021-12-04 12:44:05 -0500

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
@@ -19,14 +20,14 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
             services.AddDbContext<StoreContext>(x =>
-                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
