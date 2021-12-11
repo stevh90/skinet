488b38fef58b4bcd7a0fb0761845060fe6e0be60
Steven Heath <stevh90@gmail.com>
2021-12-11 13:08:54 -0500

    End of section 13

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using API.Helpers;
 using API.MIddleware;
 using API.Extensions;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -25,16 +26,20 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
                  x.UseSqlite(_config.GetConnectionString("DefaultConnection"))); 
-
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"),true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
             services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    opt.AddPolicy("CorsPolicy", policy =>
-                    {
-                        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
-                    });
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
                 });
+            });
 
             
                     
