ec8ef85cf238a03f4922d3b61a2d0759713e7759
Steven Heath <stevh90@gmail.com>
2021-12-11 19:48:52 -0500

    End of section 14

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BasketService } from 'src/app/basket/basket.service';
 import { IProduct } from 'src/app/shared/models/products';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
@@ -11,11 +12,14 @@ import { ShopService } from '../shop.service';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  quantity = 1;
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) 
-  {
-    this.bcService.set('@productDetails', '')
-   }
+  constructor(private shopService: ShopService,
+            private activatedRoute: ActivatedRoute,
+            private bcService: BreadcrumbService,
+            private basketService: BasketService) {
+    this.bcService.set('@productDetails', ' ')
+  }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -28,6 +32,20 @@ export class ProductDetailsComponent implements OnInit {
     }, error => {
       console.log(error);
     })
+  }
+
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
   }
 
 }
